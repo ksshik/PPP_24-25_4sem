f4a4f48ab60e93f0a95fd41bebb0a5dc579a9e8d
ksshik <ksenia.derbunovich@yandex.ru>
2025-05-22 21:29:27 +0300

    дз

---
+++ 3lab/project/app/core/auth_utils.py
@@ -0,0 +1,43 @@
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
+from fastapi import Depends, HTTPException, status  
+import jwt 
+from datetime import datetime, timedelta, timezone  
+from app.core.config import settings  
+
+security = HTTPBearer()  
+
+def create_access_token(data: dict):  # создание токена
+    to_encode = data.copy() 
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)  # время истечения
+    to_encode.update({"exp": expire})  
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)  # JWT токен с секретным ключом
+    return encoded_jwt  
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:  # проверка пароля
+    from passlib.context import CryptContext  
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")  
+    return pwd_context.verify(plain_password, hashed_password)  
+
+def get_password_hash(password: str) -> str:  # хеширование пароля
+    from passlib.context import CryptContext  
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") 
+    return pwd_context.hash(password)  
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):  # извлечение текущего пользователя
+    token = credentials.credentials  
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])  # декодируем токен с помощью секретного ключа
+        user_id: str = payload.get("sub")  #
+        if user_id is None:  
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,  # ошибка авторизации
+                detail="Invalid authentication credentials",  
+                headers={"WWW-Authenticate": "Bearer"}, 
+            )
+    except jwt.PyJWTError:  # если произошла ошибка при декодировании токена
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,  
+            detail="Invalid authentication credentials",  
+            headers={"WWW-Authenticate": "Bearer"},  
+        )
+    return user_id  
